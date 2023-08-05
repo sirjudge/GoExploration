acbeb1285ee14cdcf12283307422447d7dcce990
nico <nico.a.judge@gmail.com>
2023-08-05 10:29:19 -0400

    did some things

--- awesomeProject/awesomeproject.go
+++ awesomeProject/awesomeproject.go
@@ -14,7 +14,10 @@ func main() {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
+	printPortInfo(port)
+}
 
+func printPortInfo(port string) {
 	log.Printf("Listening on port %s", port)
 	log.Printf("Open http://localhost:%s in the browser", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
@@ -25,14 +28,14 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		_, err := fmt.Fprint(w, "home page")
 		if err != nil {
-			logError(err, r.URL.Path)
+			logError(err, r)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	} else if r.URL.Path == "/test" {
 		_, err := fmt.Fprint(w, "test page")
 		if err != nil {
-			logError(err, r.URL.Path)
+			logError(err, r)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
@@ -47,7 +50,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func RealController(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		logError(err, r.URL.Path)
+		logError(err, r)
 		return
 	}
 	key1 := r.Form.Get("key1")
@@ -55,10 +58,14 @@ func RealController(w http.ResponseWriter, r *http.Request) {
 	log.Printf("key1:%s  key2:%s", key1, key2)
 }
 
-func logError(err error, path string) {
-	log.Printf("error occurred when accessing path: %s Message: %s}", path, err)
+func logError(err error, r *http.Request) {
+	log.Printf("error occurred when accessing path: %s Message: %s}", r.URL.Path, err)
 }
 
 func logRequest(r *http.Request) {
 	log.Printf("URL Path: %s", r.URL.Path)
+}
+
+func GetRandomDogPhoto() {
+
 }
